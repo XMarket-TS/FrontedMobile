f2c15c9fb0d629c911d0f0cfb4a5f9fb1befb533
Fulano2141 <macuto2142dt@gmail.com>
2021-04-06 14:49:31 -0400

    Bloc and Navigation enhanced

--- lib/Events/NavigationEvents.dart
+++ lib/Events/NavigationEvents.dart
@@ -1,34 +1,39 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 
-abstract class NavigationEvents extends Equatable {
+abstract class NavigationEvents extends Equatable {}
 
-}
-class NavigationProductPageEvent extends NavigationEvents{
+class NavigationProductPageEvent extends NavigationEvents {
   int _branchId;
+
   // String _branchName;
   int _categorieId;
+
   NavigationProductPageEvent(this._branchId, this._categorieId);
+
   @override
-  List<Object> get props => [_branchId,_categorieId];
+  List<Object> get props => [_branchId, _categorieId];
 }
-class NavigationBranchPageEvent extends NavigationEvents{
-  @override
-  List<Object> get props => [];
-}
-class NavigationProfilePageEvent extends NavigationEvents{
+
+class NavigationBranchPageEvent extends NavigationEvents {
   @override
   List<Object> get props => [];
 }
-class NavigationCartPageEvent extends NavigationEvents{
+
+class NavigationProfilePageEvent extends NavigationEvents {
   @override
   List<Object> get props => [];
 }
-class NavigationCategoriesPageEvent extends NavigationEvents{
+
+class NavigationCartPageEvent extends NavigationEvents {
+  @override
+  List<Object> get props => [];
+}
+
+class NavigationCategoriesPageEvent extends NavigationEvents {
   int _branchId;
+
   NavigationCategoriesPageEvent(this._branchId);
 
   @override
   List<Object> get props => [_branchId];
-
 }
